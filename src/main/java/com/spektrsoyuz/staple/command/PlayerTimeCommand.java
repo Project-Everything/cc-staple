7e5106ac5947f61a1e7ca385a7a8847c1dfd6ea5
SpektrSoyuz <christies1109@gmail.com>
2024-10-12 00:59:40 -0400

    Add /playertime command

---
+++ src/main/java/com/spektrsoyuz/staple/command/PlayerTimeCommand.java
@@ -0,0 +1,71 @@
+package com.spektrsoyuz.staple.command;
+
+import com.spektrsoyuz.staple.util.StapleColor;
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@SuppressWarnings({"UnstableApiUsage"})
+
+public final class PlayerTimeCommand implements BasicCommand {
+
+    @Override
+    public void execute(@NotNull CommandSourceStack stack, @NotNull String[] args) {
+        CommandSender sender = stack.getSender();
+
+        // Check if sender is not player
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(Component.text("The console cannot use this command.").color(StapleColor.RED));
+            return;
+        }
+
+        // Check if player entered no arguments
+        if (args.length == 0) {
+            player.sendMessage(Component.text("Usage: /playertime <day/night/morning/evening/off>").color(StapleColor.GRAY));
+            return;
+        }
+
+        switch (args[0]) {
+            case "morning":
+                player.sendMessage(Component.text("Time set to morning.").color(StapleColor.GOLD));
+                player.setPlayerTime(0, false);
+                break;
+            case "day":
+                player.sendMessage(Component.text("Time set to day.").color(StapleColor.GOLD));
+                player.setPlayerTime(6000, false);
+                break;
+            case "evening":
+                player.sendMessage(Component.text("Time set to evening.").color(StapleColor.GOLD));
+                player.setPlayerTime(12000, false);
+                break;
+            case "night":
+                player.sendMessage(Component.text("Time set to night.").color(StapleColor.GOLD));
+                player.setPlayerTime(18000, false);
+                break;
+            case "off":
+                player.sendMessage(Component.text("Disabled player time.").color(StapleColor.GOLD));
+                player.setPlayerTime(Objects.requireNonNull(Bukkit.getWorld("plotworld")).getTime(), true);
+                break;
+            default:
+                try {
+                    long time = Long.parseLong(args[0]);
+                    player.sendMessage(Component.text("Time set to " + time).color(StapleColor.GOLD));
+                    player.setPlayerTime(time, true);
+                } catch (NumberFormatException e) {
+                    player.sendMessage(Component.text("Invalid time format.").color(StapleColor.RED));
+                }
+                break;
+        }
+    }
+
+    @Override
+    public @NotNull String permission() {
+        return "staple.playertime";
+    }
+}
