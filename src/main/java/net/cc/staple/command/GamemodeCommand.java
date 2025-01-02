7a0e6dace57dfd874dcdfbbf7181b0b1c10c9f6a
SpektrSoyuz <christies1109@gmail.com>
2025-01-02 17:12:25 -0500

    Fixed Gamemode command not using argument resolver

--- src/main/java/net/cc/staple/command/GamemodeCommand.java
+++ src/main/java/net/cc/staple/command/GamemodeCommand.java
@@ -2,9 +2,11 @@ package net.cc.staple.command;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
 import net.cc.staple.StapleUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -25,7 +27,7 @@ public final class GamemodeCommand {
                         .executes(this::execute0)
                         .then(Commands.argument("gamemode", ArgumentTypes.gameMode())
                                 .executes(this::execute1)
-                                .then(Commands.argument("player", ArgumentTypes.player())
+                                .then(Commands.argument("target", ArgumentTypes.player())
                                         .requires(stack -> stack.getSender().hasPermission(StapleUtil.PERMISSION_COMMAND_GAMEMODE_OTHER))
                                         .executes(this::execute2)))
                         .build(),
@@ -35,7 +37,7 @@ public final class GamemodeCommand {
         commands.register(Commands.literal("gms")
                         .requires(stack -> stack.getSender().hasPermission(StapleUtil.PERMISSION_COMMAND_GAMEMODE))
                         .executes(context -> execute3(context, GameMode.SURVIVAL))
-                        .then(Commands.argument("player", ArgumentTypes.player())
+                        .then(Commands.argument("target", ArgumentTypes.player())
                                 .executes(context -> execute4(context, GameMode.SURVIVAL)))
                         .build(),
                 "Set your gamemode to Survival");
@@ -43,7 +45,7 @@ public final class GamemodeCommand {
         commands.register(Commands.literal("gmc")
                         .requires(stack -> stack.getSender().hasPermission(StapleUtil.PERMISSION_COMMAND_GAMEMODE))
                         .executes(context -> execute3(context, GameMode.CREATIVE))
-                        .then(Commands.argument("player", ArgumentTypes.player())
+                        .then(Commands.argument("target", ArgumentTypes.player())
                                 .executes(context -> execute4(context, GameMode.CREATIVE)))
                         .build(),
                 "Set your gamemode to Creative");
@@ -51,7 +53,7 @@ public final class GamemodeCommand {
         commands.register(Commands.literal("gma")
                         .requires(stack -> stack.getSender().hasPermission(StapleUtil.PERMISSION_COMMAND_GAMEMODE))
                         .executes(context -> execute3(context, GameMode.ADVENTURE))
-                        .then(Commands.argument("player", ArgumentTypes.player())
+                        .then(Commands.argument("target", ArgumentTypes.player())
                                 .executes(context -> execute4(context, GameMode.ADVENTURE)))
                         .build(),
                 "Set your gamemode to Adventure");
@@ -59,7 +61,7 @@ public final class GamemodeCommand {
         commands.register(Commands.literal("gmsp")
                         .requires(stack -> stack.getSender().hasPermission(StapleUtil.PERMISSION_COMMAND_GAMEMODE))
                         .executes(context -> execute3(context, GameMode.SPECTATOR))
-                        .then(Commands.argument("player", ArgumentTypes.player())
+                        .then(Commands.argument("target", ArgumentTypes.player())
                                 .executes(context -> execute4(context, GameMode.SPECTATOR)))
                         .build(),
                 "Set your gamemode to Spectator");
@@ -81,31 +83,24 @@ public final class GamemodeCommand {
         CommandSender sender = context.getSource().getSender();
         if (sender instanceof Player player) {
             GameMode gameMode = context.getArgument("gamemode", GameMode.class);
-            player.setGameMode(gameMode);
-            player.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
+            setGamemode(player, gameMode);
             return Command.SINGLE_SUCCESS;
         }
         return 0;
     }
 
     private int execute2(CommandContext<CommandSourceStack> context) {
+        final PlayerSelectorArgumentResolver targetResolver = context.getArgument("target", PlayerSelectorArgumentResolver.class);
         CommandSender sender = context.getSource().getSender();
         if (sender instanceof Player player) {
             GameMode gameMode = context.getArgument("gamemode", GameMode.class);
-            Player targetPlayer = context.getArgument("player", Player.class);
-            targetPlayer.setGameMode(gameMode);
-            player.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + " for " + targetPlayer.getName() + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
-            targetPlayer.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
-            return Command.SINGLE_SUCCESS;
+            try {
+                Player target = targetResolver.resolve(context.getSource()).getFirst();
+                setGamemode(player, target, gameMode);
+                return Command.SINGLE_SUCCESS;
+            } catch (CommandSyntaxException e) {
+                context.getSource().getSender().sendMessage(Component.text(e.getMessage()).color(NamedTextColor.RED));
+            }
         }
         return 0;
     }
@@ -113,31 +108,43 @@ public final class GamemodeCommand {
     private int execute3(CommandContext<CommandSourceStack> context, GameMode gameMode) {
         CommandSender sender = context.getSource().getSender();
         if (sender instanceof Player player) {
-            player.setGameMode(gameMode);
-            player.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
+            setGamemode(player, gameMode);
             return Command.SINGLE_SUCCESS;
         }
         return 0;
     }
 
     private int execute4(CommandContext<CommandSourceStack> context, GameMode gameMode) {
+        final PlayerSelectorArgumentResolver targetResolver = context.getArgument("target", PlayerSelectorArgumentResolver.class);
         CommandSender sender = context.getSource().getSender();
         if (sender instanceof Player player) {
-            Player targetPlayer = context.getArgument("player", Player.class);
-            targetPlayer.setGameMode(gameMode);
-            player.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + " for " + targetPlayer.getName() + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
-            targetPlayer.sendMessage(Component.text()
-                    .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
-                    .color(NamedTextColor.GOLD)
-                    .build());
-            return Command.SINGLE_SUCCESS;
+            try {
+                Player target = targetResolver.resolve(context.getSource()).getFirst();
+                setGamemode(player, target, gameMode);
+                return Command.SINGLE_SUCCESS;
+            } catch (CommandSyntaxException e) {
+                context.getSource().getSender().sendMessage(Component.text(e.getMessage()).color(NamedTextColor.RED));
+            }
         }
         return 0;
+    }
+
+    private void setGamemode(Player source, GameMode gameMode) {
+        source.setGameMode(gameMode);
+        source.sendMessage(Component.text()
+                .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
+                .color(NamedTextColor.GOLD)
+                .build());
+    }
+
+    private void setGamemode(Player source, Player target, GameMode gameMode) {
+        source.sendMessage(Component.text()
+                .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + " for " + target.getName() + ".")
+                .color(NamedTextColor.GOLD)
+                .build());
+        target.sendMessage(Component.text()
+                .content("Gamemode set to " + StringUtils.capitalizeFirstLetter(gameMode.name()) + ".")
+                .color(NamedTextColor.GOLD)
+                .build());
     }
 }
